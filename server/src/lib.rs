54875de52d4173efb92378cd8fd1b7bca6e7476f
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-05-29 21:10:30 +0200

    Move user management to separate crate

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,12 +1,12 @@
-use diesel::{connection::DefaultLoadingMode, prelude::*};
+use diesel::prelude::*;
 use dotenvy::dotenv;
-use models::{NewUser, User, UserRole};
 use std::borrow::BorrowMut;
 use std::env;
 
 pub mod cli;
 pub mod models;
-pub mod schema;
+pub mod user;
+mod schema;
 
 pub struct Labman {
     // TODO: Make generic
@@ -22,70 +22,7 @@ impl Labman {
         Ok(Labman { conn: conn })
     }
 
-    pub fn user(&mut self) -> UserManager {
-        UserManager::new(self.conn.borrow_mut())
-    }
-}
-
-pub struct UserManager<'a> {
-    conn: &'a mut SqliteConnection,
-}
-
-impl<'a> UserManager<'a> {
-    pub fn new(conn: &'a mut SqliteConnection) -> Self {
-        UserManager { conn }
-    }
-
-    /// Create a user in the database
-    pub fn create(
-        &mut self,
-        name: &String,
-        role: &models::UserRole,
-    ) -> Result<models::User, diesel::result::Error> {
-        use schema::users;
-        let new_user = NewUser { name, role };
-        diesel::insert_into(users::table)
-            .values(&new_user)
-            .returning(User::as_returning())
-            .get_result(self.conn)
-    }
-
-    /// Get a user from the database
-    pub fn get(&mut self, name: &String) -> Result<models::User, diesel::result::Error> {
-        use schema::users;
-        users::table
-            .filter(users::name.eq(name))
-            .select(models::User::as_select())
-            .first(self.conn)
-    }
-
-    /// Get the users with a minimum role
-    pub fn iter<'labman>(
-        &'labman mut self,
-        min_role: &'labman UserRole,
-    ) -> Result<
-        impl Iterator<Item = Result<User, diesel::result::Error>> + 'labman,
-        diesel::result::Error,
-    > {
-        use schema::users;
-        users::table
-            .filter(users::role.ge(min_role))
-            .load_iter::<User, DefaultLoadingMode>(self.conn)
-    }
-
-    pub fn delete(&mut self, name: &String) -> Option<diesel::result::Error> {
-        use schema::users;
-        match self.get(name) {
-            Ok(user) => {
-                if let Err(err) =
-                    diesel::delete(users::table.filter(users::id.eq(user.id))).execute(self.conn)
-                {
-                    Some(err)
-                } else {
-                    None
-                }
-            }
-            Err(err) => Some(err),
-        }
+    pub fn user(&mut self) -> user::UserManager {
+        user::UserManager::new(self.conn.borrow_mut())
     }
 }
---
+++ server/src/user.rs
@@ -0,0 +1,66 @@
+use crate::models;
+use crate::schema;
+use diesel::{connection::DefaultLoadingMode, prelude::*};
+
+pub struct UserManager<'a> {
+    conn: &'a mut SqliteConnection,
+}
+
+impl<'a> UserManager<'a> {
+    pub fn new(conn: &'a mut SqliteConnection) -> Self {
+        UserManager { conn }
+    }
+
+    /// Create a user in the database
+    pub fn create(
+        &mut self,
+        name: &String,
+        role: &models::UserRole,
+    ) -> Result<models::User, diesel::result::Error> {
+        use schema::users;
+        let new_user = models::NewUser { name, role };
+        diesel::insert_into(users::table)
+            .values(&new_user)
+            .returning(models::User::as_returning())
+            .get_result(self.conn)
+    }
+
+    /// Get a user from the database
+    pub fn get(&mut self, name: &String) -> Result<models::User, diesel::result::Error> {
+        use schema::users;
+        users::table
+            .filter(users::name.eq(name))
+            .select(models::User::as_select())
+            .first(self.conn)
+    }
+
+    /// Get the users with a minimum role
+    pub fn iter<'labman>(
+        &'labman mut self,
+        min_role: &'labman models::UserRole,
+    ) -> Result<
+        impl Iterator<Item = Result<models::User, diesel::result::Error>> + 'labman,
+        diesel::result::Error,
+    > {
+        use schema::users;
+        users::table
+            .filter(users::role.ge(min_role))
+            .load_iter::<models::User, DefaultLoadingMode>(self.conn)
+    }
+
+    pub fn delete(&mut self, name: &String) -> Option<diesel::result::Error> {
+        use schema::users;
+        match self.get(name) {
+            Ok(user) => {
+                if let Err(err) =
+                    diesel::delete(users::table.filter(users::id.eq(user.id))).execute(self.conn)
+                {
+                    Some(err)
+                } else {
+                    None
+                }
+            }
+            Err(err) => Some(err),
+        }
+    }
+}
