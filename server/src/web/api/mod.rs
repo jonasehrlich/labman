4ec51295cf5460a3c7c2297a70597f6d5484011b
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2025-05-30 20:45:23 +0200

    Add JSON for pure status responses

--- server/src/web/api/mod.rs
+++ server/src/web/api/mod.rs
@@ -1,9 +1,37 @@
 use crate::core;
 use axum::routing;
+use axum::{Json, http, response};
+use serde::Serialize;
 use std::sync::Arc;
+use utoipa::ToSchema;
 
 pub fn router() -> routing::Router<Arc<core::Labman>> {
     routing::Router::new().nest("/v1", v1::router())
+}
+
+/// Represents a JSON response for HTTP status codes containing the canonical reason for the status.
+#[derive(Serialize, ToSchema)]
+struct StatusJsonResponse {
+    status: String,
+}
+
+/// Represents an HTTP status code that can be converted into a response.
+struct HttpStatus(http::StatusCode);
+
+impl From<http::StatusCode> for HttpStatus {
+    fn from(status: http::StatusCode) -> Self {
+        HttpStatus(status)
+    }
+}
+
+impl response::IntoResponse for HttpStatus {
+    fn into_response(self) -> response::Response {
+        let error_response = StatusJsonResponse {
+            status: self.0.canonical_reason().unwrap_or("Unknown").to_string(),
+        };
+
+        (self.0, Json(error_response)).into_response()
+    }
 }
 
 pub mod v1 {
@@ -28,13 +56,13 @@ pub mod v1 {
     description = "List all users",
     responses(
         (status = OK, description = "List users", body = [core::models::User]),
-        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error"),
-        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized")
+        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error", body = super::StatusJsonResponse),
+        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized", body = super::StatusJsonResponse)
     )
 )]
     async fn list_users(
         State(labman): State<Arc<core::Labman>>,
-    ) -> Result<Json<Vec<core::models::User>>, http::StatusCode> {
+    ) -> Result<Json<Vec<core::models::User>>, super::HttpStatus> {
         // TODO: Check if requesting user is authorized to list users
 
         let users = labman
@@ -49,19 +77,19 @@ pub mod v1 {
 
     #[utoipa::path(
     get,
-    path = "/users/:id",
+    path = "/users/{id}",
     description = "Get a single user",
     responses(
         (status = OK, description = "Success", body = core::models::User),
-        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error"),
-        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized"),
-        (status = http::StatusCode::NOT_FOUND, description = "User not found")
+        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error", body = super::StatusJsonResponse),
+        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized", body = super::StatusJsonResponse),
+        (status = http::StatusCode::NOT_FOUND, description = "User not found", body = super::StatusJsonResponse)
     )
 )]
     async fn get_user(
         State(labman): State<Arc<core::Labman>>,
         Path(id): Path<u32>,
-    ) -> Result<Json<core::models::User>, http::StatusCode> {
+    ) -> Result<Json<core::models::User>, super::HttpStatus> {
         // TODO: Check if requesting user is authorized to get users
         let user = labman.user().get_by_id(id).await.map_err(not_found)?;
         Ok(Json(user))
@@ -74,14 +102,14 @@ pub mod v1 {
     description = "Create a user",
     responses(
         (status = OK, description = "Success", body = core::models::User),
-        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error"),
-        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized")
+        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error", body = super::StatusJsonResponse),
+        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized", body = super::StatusJsonResponse),
     )
 )]
     async fn create_user(
         State(labman): State<Arc<core::Labman>>,
         Json(new_user): Json<core::models::NewUser>,
-    ) -> Result<(http::StatusCode, Json<core::models::User>), http::StatusCode> {
+    ) -> Result<(http::StatusCode, Json<core::models::User>), super::HttpStatus> {
         // TODO: Check if requesting user is authorized to create users
         let user = labman
             .user()
@@ -97,14 +125,14 @@ pub mod v1 {
     path = "/users/{id}",
     description = "Delete a user",
     responses(
-        (status = OK, description = "Success"),
-        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error"),
-        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized")
+        (status = OK, description = "Success", body = super::StatusJsonResponse),
+        (status = http::StatusCode::INTERNAL_SERVER_ERROR, description = "Internal server error", body = super::StatusJsonResponse),
+        (status = http::StatusCode::UNAUTHORIZED, description = "Unauthorized", body = super::StatusJsonResponse)
     )
 )]
     async fn delete_user(
         State(_labman): State<Arc<core::Labman>>,
-    ) -> Result<http::StatusCode, http::StatusCode> {
+    ) -> Result<http::StatusCode, super::HttpStatus> {
         // TODO: Check if requesting user is authorized to delete users
 
         // Placeholder for actual user deletion logic
